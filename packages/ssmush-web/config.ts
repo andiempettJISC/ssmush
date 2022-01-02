7598aff5ace77d49194e6d7b0b9a081297222a0f
Andrew Wilson <andrew.wilson-2@manchester.ac.uk>
2022-01-02 12:09:39 +0000

    reuse checking for undefined conf

--- packages/ssmush-web/config.ts
+++ packages/ssmush-web/config.ts
@@ -11,25 +11,29 @@ export class Config {
     app_name: string
     googleClientId: string | undefined
     googleClientSecret: string | undefined
-    googleOrgName: string
+    googleOrgName: string | undefined
     sessionSecret: string
     sessionMaxAge: number
     secretMinLength: number
 
     constructor() {
         this.app_name = process.env.APP_NAME || process.env.name || process.env.npm_package_name || 'ssmush-web'
-        this.googleClientId = process.env.GOOGLE_CLIENT_ID
-        this.googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
         this.sessionSecret = process.env.SESSION_SECRET || crypto.randomBytes(20).toString("hex")
         this.sessionMaxAge = Number(process.env.SESSION_MAX_AGE) || 600000
         this.secretMinLength = Number(process.env.SECRET_MIN_LENGTH) || 16
 
-        if (process.env.GOOGLE_ORG_NAME && process.env.GOOGLE_ORG_NAME !== '') {
-            this.googleOrgName = process.env.GOOGLE_ORG_NAME
+        this.googleClientId = this.isDefined(process.env.GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_ID') ? process.env.GOOGLE_CLIENT_ID : undefined
+        this.googleClientSecret = this.isDefined(process.env.GOOGLE_CLIENT_SECRET, 'GOOGLE_CLIENT_SECRET') ? process.env.GOOGLE_CLIENT_SECRET : undefined
+        this.googleOrgName = this.isDefined(process.env.GOOGLE_ORG_NAME, 'GOOGLE_ORG_NAME') ? process.env.GOOGLE_ORG_NAME : undefined
+    }
+
+    isDefined(confValue: string | undefined, confName: string) {
+
+        if (confValue && confValue !== '') {
+            return true
         } else {
-            logger.error(`GOOGLE_ORG_NAME is ${process.env.GOOGLE_ORG_NAME}`)
-            throw new Error("An google org name is undefined. Use the 'GOOGLE_ORG_NAME' env var");
-            
+            logger.error(`${confName} is ${confValue}`)
+            throw new Error(`An google org name is undefined. Use the '${confName}' env var`)
         }
     }
 }
